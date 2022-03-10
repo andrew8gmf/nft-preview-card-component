22b4695bc2d98260e52bddfc884269ed4a2ca44d
andrew8gmf <andrew8gmf@gmail.com>
2022-03-10 00:54:40 -0300

    fix(hover): put the `home` selector in the global

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
